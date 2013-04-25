ad1335b5afdc1bacffaceee6e6b1cd47ff42130e
Philip Durbin <philip_durbin@harvard.edu>
2013-04-25 13:50:50 -0400

    try reverse sort

--- buildindex.sh
+++ buildindex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OUT="index.html"
 rm $OUT
-ack -a -f | grep -v ^Makefile$ | grep -v ^buildindex.sh$ | \
+ack -a -f | grep -v ^Makefile$ | grep -v ^buildindex.sh$ | sort -r | \
 while read i
   do echo "<a href=\"$i\">$i</a><br>" >> $OUT
 done
